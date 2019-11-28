d7724d6836681fc74bb1ae3be4a035339e7edbef
Gary Clark <gclark04432@live.co.uk>
2019-11-28 14:05:37 +0000

    up-to-get-smallest-planet

--- start_point/star_system.rb
+++ start_point/star_system.rb
@@ -1,5 +1,25 @@
 class StarSystem
   attr_reader :name, :planets
 
-    
+  def initialize(name, planets)
+    @name = name
+    @planets = planets
+  end
+
+  def planet_names
+    @planets.map { |planet| planet.name()}
+  end
+
+  def get_planet_by_name(planet_name)
+    @planets.find { |planet| planet.name == planet_name }
+  end
+
+  def get_largest_planet()
+    @planets.max_by { |planet| planet.diameter}
+  end
+
+  def get_smallest_planet()
+    @planets.min_by { |planet| planet.diameter}
+  end
+
 end
